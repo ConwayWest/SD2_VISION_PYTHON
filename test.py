02f8cb9f7fdec2136f7b73e39d8307f98ba428ff
Kyle Conway <kyleconway@rocketmail.com>
2020-04-09 13:10:42 -0400

    tkinter video switcher

--- test.py
+++ test.py
@@ -3,11 +3,18 @@ import PIL.Image, PIL.ImageTk
 import cv2
 import time
 
+vid_view = 0
+
+def VideoSwitch(num):
+    global vid_view
+    vid_view = num
+
 class App:
     def __init__(self, window, window_title, video_source=0):
         self.window = window
         self.window.title(window_title)
         self.video_source = video_source
+        self.vid_view = 0
 
         # open video source (by default this will try to open the computer webcam)
         self.vid = MyVideoCapture(self.video_source)
@@ -15,6 +22,9 @@ class App:
         # Create a canvas that can fit the above video source size
         self.canvas = tkinter.Canvas(window, width = self.vid.width, heigh = self.vid.height)
         self.canvas.pack()
+        myButtonOne = tkinter.Button(window, text="Raw Video", command=lambda *args: VideoSwitch(0)).pack()
+        myButtonTwo = tkinter.Button(window, text="Canny Edge", command=lambda *args: VideoSwitch(1)).pack()
+        myButtonThree = tkinter.Button(window, text="Final", command=lambda *args: VideoSwitch(2)).pack()
 
         # After it is called once, the update method will be automatically called every delay milliseconds
         self.delay = 15
@@ -47,11 +57,18 @@ class MyVideoCapture:
         if self.vid.isOpened():
             ret, frame = self.vid.read()
             greyFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            sobel = cv2.Sobel(greyFrame, cv2.CV_64F, 1, 1, 5)
+            # sobel = cv2.Sobel(greyFrame, cv2.CV_64F, 1, 1, 5)
             edges = cv2.Canny(greyFrame, 50, 50)
-            if ret:
+
+            if ret and vid_view == 0:
                 # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            elif ret and vid_view == 1:
                 return (ret, cv2.cvtColor(edges, cv2.COLOR_BGR2RGB))
+            elif ret and vid_view == 2:
+                contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                cv2.drawContours(frame, contours, -1, (0,0,255), 2)
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             else:
                 return (ret, None)
         else:
--- vision.py
+++ vision.py
@@ -9,14 +9,23 @@ while True:
 
     greyFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # image filters/edge detection
     laplacian = cv2.Laplacian(greyFrame, cv2.CV_64F)
     sobel = cv2.Sobel(greyFrame, cv2.CV_64F, 1, 1, 5)
     edges = cv2.Canny(greyFrame, 50, 50)
+    edgesContour = cv2.Canny(frame, 50, 50)
 
+    # display orig/filter
     cv2.imshow('Original', frame)
     cv2.imshow('Laplacian', laplacian)
     cv2.imshow('Sobel', sobel)
     cv2.imshow('Edges', edges)
+
+
+    # Find/draw contours
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(frame, contours, -1, (0,0,255), 2)
+    cv2.imshow('Contours', frame)
 
     # press escape key to exit
     k = cv2.waitKey(5) & 0xFF
